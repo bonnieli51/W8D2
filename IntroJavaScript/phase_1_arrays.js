fabc390b78b5b2c475b7b81c868523c7c129a937
“Catherine <cathanders10@gmail.com>
2022-11-01 15:29:16 -0400

    justin made me sad

--- IntroJavaScript/phase_1_arrays.js
+++ IntroJavaScript/phase_1_arrays.js
@@ -1,7 +1,7 @@
-function unique (array) {
+ Array.prototype.uniq = function () {
     const uniqArray= [];
 
-    array.forEach(el => {
+    this.forEach(el => {
         if (!uniqArray.includes(el)) {
             uniqArray.push(el);
         }
@@ -9,20 +9,34 @@ function unique (array) {
     return uniqArray;
 }
 
-// console.log(unique([1,2,2,3,3,3]));
+// console.log([1,2,2,3,3,3].uniq());
 
-function twoSum (array) {
+Array.prototype.twoSum = function () {
     const twoSumArray = [];
 
-    for(let i = 0, i < array.length; i++){
-        for (let k = 0, k < array.length; k++){
-            if (array[i] + array[k] === 0) {
-                twoSumArray.push(i);
-                twoSumArray.push(k);
+    for(let i = 0; i < this.length; i++){
+        for (let k = i+1; k < this.length; k++){
+            if (this[i] + this[k] === 0) {
+                twoSumArray.push([i,k]);
             }
         }
     }
     return twoSumArray;
 }   
 
-console.log(twoSum([1,-2,2,-3,3]));
+// console.log([1, -2, 2, -3, 3].twoSum());
+
+Array.prototype.transpose = function() {
+    const transposed = [];
+
+    for (let i = 0; i < this.length; i++) {
+        let temp = [];
+        for (let k = 0; k < this[0].length; k++) {
+            temp.push([this[k][i]]);
+        }
+        transposed.push(temp);
+    }
+    return transposed;
+}
+
+console.log([[1,2,3],[4,5,6],[7,8,9]].transpose());
